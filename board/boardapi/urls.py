f54038d8526df20ba1df187fb893ffee38471fce
sjy-dv <kn2414e@gmail.com>
2021-08-30 17:35:42 +0900

    update ver

--- board/boardapi/urls.py
+++ board/boardapi/urls.py
@@ -4,4 +4,9 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^api/user/sign$', views.sign),
     url(r'^api/user/login$', views.login),
+    url(r'^api/user/auth$', views.auth),
+    url(r'^api/board/write', views.boardwrite),
+    url(r'^api/board/update', views.boardupdate),
+    url(r'^api/board/detail', views.boarddetail),
+    url(r'^api/board/delete', views.boarddelete),
 ]
--- board/boardapi/views.py
+++ board/boardapi/views.py
@@ -49,10 +49,77 @@ def login(request):
             print(rows)
             print(rows[0].userid)
             if (bcrypt.checkpw(body['password'].encode('utf-8'),rows[0].password.encode('utf-8'))) :
-                access_token = jwt.encode({'userid':rows[0].userid, 'exp' : 1000 * 60 * 60 * 365}, 'secretkey', algorithm='HS256')
+                access_token = jwt.encode({'userid':rows[0].userid}, 'secretkey', algorithm='HS256')
                 return JsonResponse({"result" : access_token}, status=status.HTTP_200_OK)
             else:
                 return JsonResponse({"result" : "password incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as ex:
+            print(ex)
+        finally:
+            cursor.close()
+
+@api_view(['POST'])
+def auth(request):
+    with connection.cursor() as cursor:
+        try:
+            body = JSONParser().parse(request)
+            decoded = jwt.decode(body['token'],'secretkey', algorithms=["HS256"])
+            print(decoded)
+            if (decoded['userid'] != None):
+                return JsonResponse({"result" : decoded['userid']}, status=status.HTTP_200_OK)
+            else:
+                return JsonResponse({"result" : "token error"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as ex:
+            print(ex)
+        finally:
+            cursor.close()
+
+@api_view(['POST'])
+def boardwrite(request):
+    with connection.cursor() as cursor:
+        try:
+            body = JSONParser().parse(request)
+            decoded = jwt.decode(body['token'],'secretkey', algorithms=["HS256"])
+            cursor.execute("insert into board(title,desc,writer) values(%s, %s, %s)",[body['title'], body['desc'], decoded['userid']])
+            return JsonResponse({"result" : "success"}, status=status.HTTP_200_OK)
+        except Exception as ex:
+            print(ex)
+        finally:
+            cursor.close()
+
+@api_view(["POST"])
+def boardupdate(request):
+    with connection.cursor() as cursor:
+        try:
+            body = JSONParser().parse(request)
+            cursor.execute("update board set title=%s, desc=%s where idx=%s",[body['title'], body['desc'], body['idx']])
+            return JsonResponse({"result" : "success"}, status=status.HTTP_200_OK)
+        except Exception as ex:
+            print(ex)
+        finally:
+            cursor.close()
+
+@api_view(["GET"])
+def boarddetail(request):
+    with connection.cursor() as cursor:
+        try:
+            #body = JSONParser().parse(request)
+            target_idx = request.GET.get('idx', None)
+            cursor.execute("select * from board where idx = %s",[target_idx])
+            rows = namedtuplefetchall(cursor)
+            return JsonResponse({"result" : rows}, status=status.HTTP_200_OK)
+        except Exception as ex:
+            print(ex)
+        finally:
+            cursor.close()
+
+@api_view(["POST"])
+def boarddelete(request):
+    with connection.cursor() as cursor:
+        try:
+            body = JSONParser().parse(request)
+            cursor.execute("delete from board where idx = %s", [body['idx']])
+            return JsonResponse({"result" : "success"}, status=status.HTTP_200_OK)
         except Exception as ex:
             print(ex)
         finally:
